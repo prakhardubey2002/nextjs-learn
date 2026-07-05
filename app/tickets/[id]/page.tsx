bb8591cb970eebac5c1a67a1d820f91efb3f74d8
Prakhar Dubey <prakhardubey2002@gmail.com>
2026-07-06 00:28:46 +0530

    statis site generation

--- app/tickets/[id]/page.tsx
+++ app/tickets/[id]/page.tsx
@@ -1,10 +1,20 @@
 import TicketDetails from "./TicketDetails"
-
+import type { Ticket } from "@/types"
+//The generateStaticParams function can be used in combination with dynamic route segments to statically generate routes at build time instead of on-demand at request time.
+// will generate all the tickets pages at build time also will revalidate as needed
+// (SSG) Static Site Generation -    prerendered as static HTML (uses generateStaticParams)
+export async function generateStaticParams() {
+  const res = await fetch("http://localhost:4000/tickets")// run on build time thats why native fetch instead of useswr
+  const tickets: Ticket[] = await res.json()
+  return tickets.map((ticket) => ({
+    id: ticket.id,
+  }))
+}
 export default async function TicketPage({
-  params,
+    params,
 }: {
-  params: Promise<{ id: string }>
+    params: Promise<{ id: string }>
 }) {
-  const { id } = await params
-  return <TicketDetails id={id} />
+    const { id } = await params
+    return <TicketDetails id={id} />
 }
