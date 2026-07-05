10d10e44aedb93b3d0ceac24a6d9ba6fbcaa66f8
Prakhar Dubey <prakhardubey2002@gmail.com>
2026-07-05 18:06:49 +0530

    css,image,fonts

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,10 +1,12 @@
 import Link from 'next/link'
-import React from 'react'
+import Logo from '@/assets/images/logo.png'
+import Image from 'next/image'
 
 const Navbar = () => {
     return (
         <nav>
-            <ul>
+            <ul className='flex w-[80vw] mx-auto justify-start items-center gap-4' >
+                <Image src={Logo} alt="Dojo Help Desk" placeholder='blur' width={100} height={100} quality={100} />
                 <li><Link href="/">Home</Link></li>
                 <li><Link href="/news">News</Link></li>
                 <li><Link href="/tickets">Tickets</Link></li>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,11 @@
-import Navbar from "@/app/components/Navbar";
+import Navbar from "@/components/Navbar";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Rubik } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const rubik = Rubik({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["400", "700"],
 });
 
 export const metadata: Metadata = {
@@ -26,11 +21,13 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${rubik.className} h-full antialiased`}
     >
-      <h1>Dojo Help Desk</h1>
-      <Navbar />
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <h1 className="text-4xl font-bold">Dojo Help Desk</h1>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
