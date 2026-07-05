85feb99c6cdffef64015aea2a53df52d779d248c
Prakhar Dubey <prakhardubey2002@gmail.com>
2026-07-05 17:35:08 +0530

    page routes

---
+++ app/news/page.tsx
@@ -0,0 +1,16 @@
+export default function News() {
+    return <>
+        <div className="main">
+            <ul>
+                <li><a href="#">News</a></li>
+                <li><a href="#">News</a></li>
+            </ul>
+        </div>
+        <div className="main">
+            <ul>
+                <li><a href="#">News</a></li>
+                <li><a href="#">News</a></li>
+            </ul>
+        </div>
+    </>
+}
